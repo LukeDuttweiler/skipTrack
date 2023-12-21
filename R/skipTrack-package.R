64d14c2281cd426a6f9998f7f47f7b6e508e85f2
LukeDuttweiler <luke.duttweiler16@houghton.edu>
2023-12-21 14:33:59 -0500

    Added package documentation with imported function. Also commented out skipTrackMCMC example as it runs every time we use check() which slows the whole thing down.

---
+++ R/skipTrack-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats dlnorm
+#' @importFrom stats rbeta
+#' @importFrom stats rgamma
+#' @importFrom stats rgeom
+#' @importFrom stats rlnorm
+#' @importFrom stats rnorm
+#' @importFrom stats rpois
+## usethis namespace: end
+NULL
