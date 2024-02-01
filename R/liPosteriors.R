3a243328b00d120fa0e405bdd29951782abab384
LukeDuttweiler <luke.duttweiler16@houghton.edu>
2024-02-01 15:40:42 -0500

    Added MH draw system for Pi_i in li algorithm.

--- R/liPosteriors.R
+++ R/liPosteriors.R
@@ -1,6 +1,22 @@
-#This document holds all of the functions that provide random draws from full conditional
-#posteriors for the 'Li' algorithm.
-
+#' Compute random draw from full conditional posterior for lambda_i in Li algorithm.
+#'
+#' This function calculates a random draw from the full conditional posterior distribution
+#' for lambda_i in the Li algorithm, given the observed values y_ij, the indicators s_ij,
+#' and the prior hyperparameters priorK and priorG.
+#'
+#' @param yij Vector of observed values for lambda_i.
+#' @param sij Vector of indicators for lambda_i.
+#' @param priorK Prior hyperparameter for K.
+#' @param priorG Prior hyperparameter for G.
+#'
+#' @return A random draw from the posterior distribution of lambda_i.
+#'
+#' @examples
+#' yij <- c(31, 62, 31)
+#' sij <- c(0, 1, 0)
+#' priorK <- 180
+#' priorG <- 6
+#' postLambdai(yij, sij, priorK, priorG)
 postLambdai <- function(yij, sij, priorK, priorG){
   #n is the number of sampled values
   n <- length(yij)
@@ -13,6 +29,52 @@ postLambdai <- function(yij, sij, priorK, priorG){
   return(rgamma(1, shape = postK, rate = postG))
 }
 
+#Function to compute random draw from posterior for pi_i in Li algorithm.
+#This one requires an MH step. Using the posterior of true geometric from the calculated hyperparameters as a proposal
+
+postPii <- function(sij, currentPii, priorA, priorB, S){
+  #n is the number of cycles for this individual
+  n <- length(sij)
+  postA <- priorA + sum(sij)
+  postB <- priorB + n
+
+  #Sample a possible pii
+  possPii <- rbeta(1, postA, postB)
+
+  #Eval M-H prob
+  qOld <- dbeta(currentPii, postA, postB)
+  qNew <- dbeta(currentPii, postA, postB)
+  pOld <- exp(sum(log((currentPii^sij)*(1-currentPii)/(1-currentPii^(S+1)))))
+  pNew <- exp(sum(log((possPii^sij)*(1-possPii)/(1-possPii^(S+1)))))
+  a <- min(1, (pNew/pOld)*(qOld/qNew))
+
+  #Flip coin to return new or old
+  if(as.logical(rbinom(1, 1, a))){
+    return(possPii)
+  }else{
+    return(currentPii)
+  }
+}
+
+#' Compute random draw from full conditional posterior for s_ij in Li algorithm.
+#'
+#' This function calculates a random draw from the full conditional posterior distribution
+#' for s_ij in the Li algorithm, given the observed values yijs, the parameter pi,
+#' the lambda_i value, and the truncation level S.
+#'
+#' @param yijs Vector of observed values for s_ij.
+#' @param pii Probability parameter pi.
+#' @param lambdai Value of lambda_i.
+#' @param S Truncation level.
+#'
+#' @return A random draw from the posterior distribution of s_ij.
+#'
+#' @examples
+#' yijs <- c(31, 62, 31)
+#' pii <- 0.2
+#' lambdai <- 30
+#' S <- 5
+#' postSij(yijs, pii, lambdai, S)
 postSij <- function(yijs, pii, lambdai, S){
   probs <- sapply(0:S, function(s){
     #Get truncated geometric probability
